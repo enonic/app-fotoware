084fc8f3ae73613754bbec4907b82f4c3c94c435
Christian Westgaard <comlock@gmail.com>
2024-10-29 14:40:35 +0100

    Fix #170 Upgrade to new lib-static

--- src/main/resources/admin/tools/fotoware/fotoware.ts
+++ src/main/resources/admin/tools/fotoware/fotoware.ts
@@ -1,9 +1,9 @@
 import type {Request} from '/lib/xp/Request';
 
 
-//import {toStr} from '@enonic/js-utils';
-// @ts-expect-error TS2307: Cannot find module '/lib/enonic/static' or its corresponding type declarations.
-import {buildGetter} from '/lib/enonic/static';
+// import {toStr} from '@enonic/js-utils';
+import endsWith from '@enonic/js-utils/string/endsWith'
+import {requestHandler} from '/lib/enonic/static';
 // @ts-expect-error TS2307: Cannot find module '/lib/license' or its corresponding type declarations.
 import {validateLicense} from '/lib/license';
 // @ts-expect-error TS2307: Cannot find module '/lib/router' or its corresponding type declarations.
@@ -17,10 +17,7 @@ import {
 	getResource,
 	readText
 } from '/lib/xp/io';
-import {
-	assetUrl,
-	serviceUrl as getServiceUrl
-} from '/lib/xp/portal';
+import {serviceUrl as getServiceUrl} from '/lib/xp/portal';
 
 import {post} from './post';
 import {getUploadLicenseForm} from './getUploadLicenseForm';
@@ -47,36 +44,40 @@ router.post('', (r: Request) => post(r));
 router.get('/uploadLicense', (/*r: Request*/) => getUploadLicenseForm(/*r*/));
 router.post('/uploadLicense', (r: Request) => postUploadLicense(r));
 
-const getAsset = buildGetter('assets', {
-	//cacheControl: 'public, max-age=31536000, immutable', // This is the default
-	//cacheControl: 'public, no-transform, max-age=31536000',
-	//cacheControl: false,
-	//cacheControl: 'must-revalidate', // 304
-	contentType: {
-		css: 'text/css',
-		ico: 'image/x-icon',
-		js: 'application/javascript',
-		svg: 'image/svg+xml',
-
-		woff: 'font/woff',
-		//woff: 'application/font-woff',
-		//woff: 'application/octet-stream',
-
-		woff2: 'font/woff2'
-		//woff2: 'application/octet-stream'
-	},
-	//etag: false,
-	etag: true//,
-	/*getCleanPath: (request) => {
-		//log.debug(`request:${toStr(request)}`);
-		if (!request.rawPath.startsWith('/admin/tool/com.enonic.app.fotoware/fotoware/')) {
-			log.error(`request.rawPath:${toStr(request.rawPath)}`);
-			throw new Error('Ooops');
-		}
-		return request.rawPath.substring('/admin/tool/com.enonic.app.fotoware/fotoware/'.length);
-	},*/
-	//throwErrors: true
-});
+const getAsset = (request: Request) => requestHandler(
+	request,
+	{
+		//cacheControl: 'public, max-age=31536000, immutable', // This is the default
+		//cacheControl: 'public, no-transform, max-age=31536000',
+		//cacheControl: false,
+		//cacheControl: 'must-revalidate', // 304
+		contentType: ({
+			path,
+			// resource
+		}) => {
+			if (!path) { return 'octet/stream'; }
+			if (endsWith(path, '.js'   )) { return 'application/javascript'; }
+			if (endsWith(path, '.css'  )) { return 'text/css'; }
+			if (endsWith(path, '.ico'  )) { return 'image/x-icon'; }
+			if (endsWith(path, '.woff2')) { return 'font/woff2'; }
+			if (endsWith(path, '.woff' )) { return 'font/woff'; }
+			if (endsWith(path, '.ttf'  )) { return 'font/ttf'; }
+			if (endsWith(path, '.eot'  )) { return 'application/vnd.ms-fontobject'; }
+			if (endsWith(path, '.svg'  )) { return 'image/svg+xml'; }
+			return 'octet/stream';
+		},
+		/*getCleanPath: (request) => {
+			//log.debug(`request:${toStr(request)}`);
+			if (!request.rawPath.startsWith('/admin/tool/com.enonic.app.fotoware/fotoware/')) {
+				log.error(`request.rawPath:${toStr(request.rawPath)}`);
+				throw new Error('Ooops');
+			}
+			return request.rawPath.substring('/admin/tool/com.enonic.app.fotoware/fotoware/'.length);
+		},*/
+		//throwErrors: true
+		root: 'assets',
+	}
+);
 router.get('/admin/{path:.+}', (r: Request) => getAsset(r));
 router.get('/fonts/{path:.+}', (r: Request) => getAsset(r));
 router.get('/images/{path:.+}', (r: Request) => getAsset(r));
@@ -96,9 +97,6 @@ function get(/*request*/) {
 		)
 		: 'Unlicensed!';
 
-
-
-	const assetsUrl = assetUrl({path: ''});
 	return {
 		body: `<!DOCTYPE html>
 <html>
@@ -110,31 +108,14 @@ function get(/*request*/) {
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<meta name="theme-color" content="#ffffff">
 
-		<!--script type="text/javascript" src="${assetUrl({path: `js/react/react.${REACT_MODE}.js`})}"></script-->
 		<script type="text/javascript" src="fotoware/js/react/react.${REACT_MODE}.js"></script>
-
-		<!--script type="text/javascript" src="${assetUrl({path: `js/react-dom/react-dom.${REACT_MODE}.js`})}"></script-->
 		<script type="text/javascript" src="fotoware/js/react-dom/react-dom.${REACT_MODE}.js"></script>
-
-		<!--script type="text/javascript" src="${assetUrl({path: `js/@material-ui/material-ui.${REACT_MODE}.js`})}"></script-->
 		<script type="text/javascript" src="fotoware/js/@material-ui/material-ui.${REACT_MODE}.js"></script>
-
-		<!--script type="text/javascript" src="${assetUrl({path: 'js/@babel/standalone/babel.min.js'})}"></script-->
 		<script type="text/javascript" src="fotoware/js/@babel/standalone/babel.min.js"></script>
-
-		<!--link rel="stylesheet" href="${assetUrl({path: 'fonts/fonts.css'})}" /-->
 		<link rel="stylesheet" href="fotoware/fonts/fonts.css" />
-
-		<!--link rel="shortcut icon" href="${assetsUrl}/images/ico/fotoware.ico"-->
 		<link rel="shortcut icon" href="fotoware/images/ico/fotoware.ico">
-
-		<!--link rel="stylesheet" type="text/css" href="${assetsUrl}/admin/common/styles/lib.css"-->
 		<link rel="stylesheet" type="text/css" href="fotoware/admin/common/styles/lib.css">
-
-		<!--script defer src="${assetsUrl}/admin/common/js/lib.js" type="text/javascript"></script-->
 		<script defer src="fotoware/admin/common/js/lib.js" type="text/javascript"></script>
-
-		<!--script type="text/javascript" src="${assetUrl({path: 'js/moment/moment-with-locales.min.js'})}"></script-->
 		<script type="text/javascript" src="fotoware/js/moment/moment-with-locales.min.js"></script>
 
 		<style>
@@ -156,7 +137,6 @@ function get(/*request*/) {
 	<body>
 		<div class="appbar">
 			<div class="home-button app-icon system-info-button">
-				<!--img class="app-icon" src="${assetsUrl}/images/svg/fotoware-white.svg" style="height: 15px;margin-top: 15px;width: auto;"/-->
 				<img class="app-icon" src="fotoware/images/svg/fotoware-white.svg" style="height: 15px;margin-top: 15px;width: auto;"/>
 				<span class="app-name">FotoWare Admin</span>
 			</div>
